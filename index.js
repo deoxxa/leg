6958b82849acf713c1763626dcf2e72c3e5c5d38
deoxxa <deoxxa@fknsrs.biz>
2013-09-10 08:39:06 +1000

    i think this is nicer...

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
 module.exports = function leg(stream) {
   stream = stream || process.stderr;
 
-  var createLog = function createLog(currentNamespace) {
-    currentNamespace = currentNamespace || [];
+  var _log = function _log(level, summary, info) {
+    info = this.namespace.slice().reverse().reduce(function(i, v) {
+      var o = {};
 
-    var log = function log(level, summary, info) {
-      info = currentNamespace.slice().reverse().reduce(function(i, v) {
-        var o = {};
+      o[v] = i || null;
 
-        o[v] = i || null;
+      return o;
+    }, info);
 
-        return o;
-      }, info);
+    summary = [summary].concat(this.namespace.slice().reverse().map(function(e) {
+      return "[" + e + "]";
+    })).join(" ");
 
-      summary = [summary].concat(currentNamespace.slice().reverse().map(function(e) {
-        return "[" + e + "]";
-      })).join(" ");
+    stream.write(JSON.stringify([
+      (new Date()).toISOString(),
+      level.toUpperCase(),
+      summary,
+      info,
+    ]) + "\n");
+  };
 
-      stream.write(JSON.stringify([
-        (new Date()).toISOString(),
-        level.toUpperCase(),
-        summary,
-        info,
-      ]) + "\n");
+  ["debug", "info", "warn", "error"].forEach(function(level) {
+    _log[level] = function() {
+      return this.apply(this, [level].concat([].slice.call(arguments)));
     };
+  });
 
-    ["debug", "info", "warn", "error"].forEach(function(level) {
-      log[level] = log.bind(null, level);
+  var createLog = function createLog(namespace, parent) {
+    var ctx = Object.create(parent || null, {
+      namespace: {
+        value: namespace || [],
+      },
     });
 
-    log.namespace = function namespace(extraNamespace) {
-      return createLog(currentNamespace.slice().concat([extraNamespace]));
+    var log = function log() {
+      return _log.apply(ctx, arguments);
+    };
+
+    log.__proto__ = _log;
+
+    log.namespace = function namespace(newNamespace) {
+      return createLog(ctx.namespace.slice().concat([newNamespace]), this);
     };
 
     return log;
